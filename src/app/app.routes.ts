6f7bac68ab7a448442576643b34e422a58276588
luz21-brownie <luzclaritacochachi21@gmail.com>
2025-10-23 22:47:00 -0500

    inicio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './inicio/inicio';
+import { Beneficios } from './beneficios/beneficios';
+import { Planes } from './planes/planes';
+import { Testimonios } from './testimonios/testimonios';
+import { CentroDeAyuda } from './centro-de-ayuda/centro-de-ayuda';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+  { path: 'inicio', component: Inicio },
+  { path: 'beneficios', component: Beneficios },
+  { path: 'planes', component: Planes },
+  { path: 'testimonios', component: Testimonios },
+  { path: 'centro-de-ayuda', component: CentroDeAyuda }
+];
