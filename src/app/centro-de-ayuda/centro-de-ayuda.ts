d43016d3d0b75ea9814d9d3e00d6473353581e2c
Melanie Oliva <mel.olivac@gmail.com>
2025-10-23 20:47:42 -0500

    Agregando componentes que corresponde a la pagina web fachada

---
+++ src/app/centro-de-ayuda/centro-de-ayuda.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-centro-de-ayuda',
+  imports: [],
+  templateUrl: './centro-de-ayuda.html',
+  styleUrl: './centro-de-ayuda.css',
+})
+export class CentroDeAyuda {
+
+}
