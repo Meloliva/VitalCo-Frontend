637284b4f7c6f6ae02d14bcc9f74fa2776e57459
AllisG <alligalindo30@gmail.com>
2025-11-03 12:21:42 -0500

    modificacion

--- src/app/layouts/public-layout/public-layout.ts
+++ src/app/layouts/public-layout/public-layout.ts
@@ -27,7 +27,7 @@ export class PublicLayout {
   }
 
   private checkRoute(url: string) {
-    const hiddenRoutes = ['/registro', '/perfil','iniciarsesion'];
+    const hiddenRoutes = ['/registro', '/perfil','iniciarsesion','/recetas'];
     this.showHeaderFooter = !hiddenRoutes.some(route => url.includes(route));
 
   }
