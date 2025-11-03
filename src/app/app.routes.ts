d82cef2ab1d9a9f55bf62d54ad00b82d6b99f2d9
luz21-brownie <luzclaritacochachi21@gmail.com>
2025-11-02 23:29:55 -0500

    cambios en la ruta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { PublicLayout } from './layouts/public-layout/public-layout';
 import { PrivateLayout } from './layouts/private-layout/private-layout';
+import {Iniciarsesion} from './iniciarsecion/iniciarsecion';
 
 export const routes: Routes = [
   {
@@ -13,7 +14,7 @@ export const routes: Routes = [
       { path: 'planes', loadComponent: () => import('./planes/planes').then(m => m.Planes) },
       { path: 'testimonios', loadComponent: () => import('./testimonios/testimonios').then(m => m.Testimonios) },
       { path: 'centro-de-ayuda', loadComponent: () => import('./centro-de-ayuda/centro-de-ayuda').then(m => m.CentroDeAyuda) },
-      { path: 'iniciosecion', loadComponent: () => import('./iniciarsecion/iniciarsecion').then(m => m.IniciarSecion) },
+      { path: 'iniciosecion', loadComponent: () => import('./iniciarsecion/iniciarsecion').then(m => m.Iniciarsesion) },
       {path: 'registro', loadComponent: () => import('./registro/registro').then(m => m.Registro) },
       {
         path: 'perfil',
