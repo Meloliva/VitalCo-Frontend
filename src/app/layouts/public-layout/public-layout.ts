6f72a619f084cc85e57677da041712b1c5c16bdc
luz21-brownie <luzclaritacochachi21@gmail.com>
2025-11-02 23:04:51 -0500

    ocultar header y footer

--- src/app/layouts/public-layout/public-layout.ts
+++ src/app/layouts/public-layout/public-layout.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
-import {RouterLink, RouterOutlet} from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { NgIf } from '@angular/common';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-public-layout',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, NgIf],
   templateUrl: './public-layout.html',
   styleUrl: './public-layout.css',
 })
 export class PublicLayout {
+  showHeaderFooter: boolean = true;
 
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.checkRoute(this.router.url);
+
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      this.checkRoute(event.urlAfterRedirects);
+    });
+  }
+
+  private checkRoute(url: string) {
+    const hiddenRoutes = ['/registro', '/iniciarsesion', '/perfil'];
+    this.showHeaderFooter = !hiddenRoutes.some(route => url.includes(route));
+  }
 }
