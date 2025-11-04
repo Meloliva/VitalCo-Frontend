0a8bbbaf091a86b6809531cbb0624886ce40c7b2
Melanie Oliva <mel.olivac@gmail.com>
2025-11-04 02:31:54 -0500

    Creando componente cambiar plan

---
+++ src/app/cambiar-plan/cambiar-plan.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cambiar-plan',
+  imports: [],
+  templateUrl: './cambiar-plan.html',
+  styleUrl: './cambiar-plan.css',
+})
+export class CambiarPlan {
+
+}
