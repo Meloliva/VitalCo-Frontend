952f2c5b97670d93b293499da59ac362465da9b5
Maripareja <mari.pavi.06@gmail.com>
2025-11-10 19:12:18 -0500

    Seleccion registro con angular material

--- src/app/seleccion-registro/seleccion-registro.ts
+++ src/app/seleccion-registro/seleccion-registro.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-seleccion-registro',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './seleccion-registro.html',
   styleUrl: './seleccion-registro.css'
 })
@@ -13,7 +21,6 @@ export class SeleccionRegistro {
   constructor(private router: Router) {}
 
   registrarPaciente() {
-    // Por ahora solo muestra un mensaje, después implementarás esta ruta
     this.router.navigate(['/registro-usuario']);
   }
 
